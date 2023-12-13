1d31f47826736d16fa04e28c551b42f9e4e8847f
wangxiaojie <wangxiaojie.caict.ac.cn>
2023-12-13 17:34:32 +0800

    解决babel冲突

--- did-tool/packages/key-manager/lib/index.js
+++ did-tool/packages/key-manager/lib/index.js
@@ -1,2 +1,13 @@
 
-export {Ed25519VerificationKey2020} from './Ed25519VerificationKey2020.js';
+import {Ed25519VerificationKey2020} from './Ed25519VerificationKey2020.js';
+import PluginInterface from '../../tool/plugin-interface.js'
+export class KeyManager extends PluginInterface {
+    Generate({seed, ...keyPairOptions} = {}) {
+        return Ed25519VerificationKey2020.generate({seed, ...keyPairOptions})
+    }
+    Export({publicKey = false, privateKey = false, includeContext = false } = {}) {
+         return Ed25519VerificationKey2020.export({publicKey: publicKey, privateKey: privateKey, includeContext: includeContext})
+    }
+
+
+}
