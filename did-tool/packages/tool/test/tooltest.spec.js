7c2bd39e998788d1200ffe836fc4774b3fa1b11c
wangxiaojie <wangxiaojie.caict.ac.cn>
2023-12-22 11:17:27 +0800

    编写key-store和did-manage测试用例

--- did-tool/packages/tool/test/tooltest.spec.js
+++ did-tool/packages/tool/test/tooltest.spec.js
@@ -1,7 +1,8 @@
 // index.js or any other entry point
 import MainApplication from '../main-application.js';
 import KeyManager from 'key-manager/lib/index.js';
-import DidStore from 'data-store/lib/index.js';
+import {DidStore, KeyStoreManager} from 'data-store/lib/index.js';
+import {DidManager} from "did-manage/lib/index.js";
 import chai from "chai";
 const should = chai.should();
 const {expect} = chai;
@@ -10,9 +11,13 @@ import {seed} from "key-manager/lib/_tests_/mock-data.js";
 const mainApp = new MainApplication();
 const km = new KeyManager()
 const ds = new DidStore()
+const ksm = new KeyStoreManager()
+const dm = new DidManager()
 mainApp.registerPlugin(km);
 mainApp.registerPlugin(ds);
-describe('generate', () => {
+mainApp.registerPlugin(ksm);
+mainApp.registerPlugin(dm);
+describe('key-manager test', () => {
     it('should generate a key pair', async () => {
         const generateResult1 = await mainApp.executeAllPluginsMethods(km, 'Generate');
         console.log('generateResult1:', generateResult1)
@@ -22,21 +27,15 @@ describe('generate', () => {
         const generateResult2 = await mainApp.executeAllPluginsMethods(km, 'Generate', {seed});
         console.log('generateResult2:', generateResult2)
     });
-});
-
-describe('export', () => {
     it('export', async () => {
         const seedBytes = (new TextEncoder()).encode(seed).slice(0, 32);
         const keyPair = await mainApp.executeAllPluginsMethods(km, 'Generate', {seed:seedBytes,controller: 'did:example:1234'});
         console.log('keyPair:', keyPair);
         const exported = await keyPair.export({
-          publicKey: true, privateKey: true
+            publicKey: true, privateKey: true
         });
         console.log('exported:', exported);
     });
-});
-
-describe('singer', () => {
     it('singer', async () => {
         const seedBytes = (new TextEncoder()).encode(seed).slice(0, 32);
         const keyPair = await mainApp.executeAllPluginsMethods(km, 'Generate', {seed:seedBytes,controller: 'did:example:1234'});
@@ -48,8 +47,7 @@ describe('singer', () => {
         console.log('verifyResult:', verifyResult);
     });
 });
-
-describe('didStore Test', () => {
+describe('didStore test', () => {
     // 正常JSON数据
     const jsonData = {
         "@context": [
@@ -111,10 +109,219 @@ describe('didStore Test', () => {
     };
     it('save a DidDocument to the database', async () => {
         // 调用保存函数
-        const saveDidDocument = await mainApp.executeAllPluginsMethods(ds, 'ImportDID', {jsonData});
+        const saveDidDocument = await mainApp.executeAllPluginsMethods(ds, 'ImportDID', jsonData);
         expect(saveDidDocument.errorCode).to.equal(0);
     });
+    it('get a DidDocument to the database', async () => {
+        // 从数据库中检索保存的 DidDocument
+        const retrievedDidDocument = await mainApp.executeAllPluginsMethods(ds, 'GetDID', jsonData.id);
+        console.log('retrievedDidDocument:', retrievedDidDocument.data.didDocument);
+        // 断言
+        expect(retrievedDidDocument.errorCode).to.equal(0);
+    });
+    it('update a DidDocument to the database', async () => {
+        jsonData.created = "2020-05-10T06:23:38Z";
+        // 调用修改函数
+        const saveDidDocument = await mainApp.executeAllPluginsMethods(ds, 'UpdateDID', jsonData);
+        expect(saveDidDocument.errorCode).to.equal(0);
+    });
+    it('query DidDocument List', async () => {
+        // 从数据库中检索保存的 DidDocument
+        const retrievedDidDocument = await mainApp.executeAllPluginsMethods(ds, 'ListDIDs');
+        console.log('DidList:', retrievedDidDocument);
+        // 断言
+        expect(retrievedDidDocument.errorCode).to.equal(0);
+    });
+    it('delete DidDocument for BID', async () => {
+        // 从数据库中删除 DidDocument
+        console.log('jsonData.id:', jsonData.id);
+        const retrievedDidDocument = await mainApp.executeAllPluginsMethods(ds, 'DeleteDID', jsonData.id);
+        console.log('retrievedDidDocument:', retrievedDidDocument);
+        // 断言
+        expect(retrievedDidDocument.errorCode).to.equal(0);
+    });
 });
+describe('keyStore test', () => {
+
+    // 正常JSON数据
+    const jsonData = {
+        "id": 'did:example:1234#z6Mkpw72M9suPCBv48X2Xj4YKZJH9W7wzEK1aS6JioKSo89f',
+        "type": 'Ed25519VerificationKey2020',
+        "controller": 'did:example:1234',
+        "publicKeyMultibase": 'z6Mkpw72M9suPCBv48X2Xj4YKZJH9W7wzEK1aS6JioKSo89C',
+        "privateKeyMultibase":'zrv1mHUXWkWUpThaapTt8tkxSotE1iSRRuPNarhs3vTn2z61hQESuKXG7zGQsePB7JHdjaCzPZmBkkqULLvoLHoD82a',
+        "revoked": '2020-12-16T16:00:00Z'
+    };
+
+    // 错误JSON数据
+    const jsonDataError = {
+        "id": 'did:example:1234#z6Mkpw72M9suPCBv48X2Xj4YKZJH9W7wzEK1aS6JioKSo89FS',
+        "type": 'Ed25519VerificationKey2020',
+        "controller": '',
+        "publicKeyMultibase": 'z6Mkpw72M9suPCBv48X2Xj4YKZJH9W7wzEK1aS6JioKSo89C',
+        "privateKeyMultibase":'zrv1mHUXWkWUpThaapTt8tkxSotE1iSRRuPNarhs3vTn2z61hQESuKXG7zGQsePB7JHdjaCzPZmBkkqULLvoLHoD82a',
+        "revoked": '2020-12-16T16:00:00Z'
+    };
+
+    const password = "bif8888";
+
+    it('save key to the database', async () => {
+        // 调用保存函数
+        const saveKey = await mainApp.executeAllPluginsMethods(ksm, 'ImportKey', jsonData,password);
+        expect(saveKey.errorCode).to.equal(0);
+    });
+
+    it('get Key test', async () => {
+        // 从数据库中检索保存的 Key
+        const retrievedKey = await mainApp.executeAllPluginsMethods(ksm, 'GetKey', jsonData.id, password);
+        console.log("======>retrievedKey:", retrievedKey);
+        // 断言s
+        expect(retrievedKey.errorCode).to.equal(0);
+    });
+
+    it('update a Key to the database', async () => {
+        jsonData.controller = "did:example:12345678910";
+        // 调用修改函数
+        const saveKey = await mainApp.executeAllPluginsMethods(ksm, 'UpdateKey', jsonData, password);
+        expect(saveKey.errorCode).to.equal(0);
+    });
+
+    it('query Key List', async () => {
+        // 从数据库中检索保存的 Key
+        const retrievedKey = await mainApp.executeAllPluginsMethods(ksm, 'ListKeys');
+        console.log('DidList:', retrievedKey);
+        // 断言
+        expect(retrievedKey.errorCode).to.equal(0);
+    });
+
+    it('delete Key for Kid', async () => {
+        // 从数据库中检索保存的 Key
+        const retrievedKey = await mainApp.executeAllPluginsMethods(ksm, 'DeleteKey', jsonData.id);
+        // 断言
+        expect(retrievedKey.errorCode).to.equal(0);
+    });
+});
+describe('didManage Test', () => {
+    // 正常JSON数据
+    const jsonData = {
+        "did": "did:fake:receiverWithMediation67",
+        "keys": [
+            {
+                "type": "Ed25519",
+                "kid": "didcomm-receiverWithMediation2Key-1",
+                "publicKeyHex": "b162e405b6485eff8a57932429b192ec4de13c06813e9028a7cdadf0e2703636",
+                "privateKeyHex": "19ed9b6949cfd0f9a57e30f0927839a985fa699491886ebcdda6a954d869732ab162e405b6485eff8a57932429b192ec4de13c06813e9028a7cdadf0e2703636"
+            }
+        ],
+        "authentication": [
+            "did:bid:efYGggWARD5GN5TMmMcxm7XRa9DJXRLE#z6Mkpw72M9suPCBv48X2Xj4YKZJH9W7wzEK1aS6JioKSo89C"
+        ],
+        "extension": {
+            "recovery": ["did:bid:efnVUgqQFfYeu97ABf6sGm3WFtVXHZB2#key-2"],
+            "ttl": 86400,
+            "delegateSign ": {
+                "signer": "did:bid:efJgt44mNDewKK1VEN454R17cjso3mSG#key-1",
+                "signatureValue":"eyJhbGciOiJSUzI1NiIsImI2NCI6ZmFsc2UsImNyaXQiOlsiYjY0Il19"
+            },
+            "type": 206,
+            "attributes": [{
+                "key": "contract",
+                "desc": "智能合约地址",
+                "encrypt": "false",
+                "format": "text",
+                "value": "did:bid:efJgt44mNDewKK1VEN454R17cjso3mSG"
+            }]
+        },
+        "services": [
+            {
+                "id": "msg4",
+                "type": "DIDCommMessaging",
+                "serviceEndpoint": [
+                    {
+                        "uri": "http://localhost:3002/messaging",
+                        "routingKeys": [
+                            "${mediator2.did}#${mediator2.keys[0].kid}",
+                            "${mediator.did}#${mediator.keys[0].kid}"
+                        ]
+                    }
+                ]
+            }
+        ],
+        "provider": "did:fake"
+    };
+
+    it('didManagerCreate Test', async () => {
+        // 调用保存函数
+        const didDocumentCreate = await mainApp.executeAllPluginsMethods(dm, 'DidManagerCreate');
+        expect(didDocumentCreate.errorCode).to.equal(0);
+    });
+
+    it('didManagerImport Test', async () => {
+        // 调用保存函数
+        const didDocumentImport = await mainApp.executeAllPluginsMethods(dm, 'DidManagerImport',jsonData);
+        expect(didDocumentImport.errorCode).to.equal(0);
+    });
+
+    it('update a DidDocument', async () => {
+        jsonData.services.id = "did:fake:receiverWithMediation2";
+        // 调用修改函数
+        const saveDidDocument = await mainApp.executeAllPluginsMethods(dm, 'DidManagerUpdate',jsonData);
+        expect(saveDidDocument.errorCode).to.equal(0);
+    });
+
+    it('didManageraddKey it', async () => {
+        const result = await mainApp.executeAllPluginsMethods(dm, 'DidManageraddKey',jsonData.did,"did:bid:efYGggWARD5GN5TMmMcxm7XRa9DJXRPP#z6Mkpw72M9suPCBv48X2Xj4YKZJH9W7wzEK1aS6JioKSo89C");
+        // 断言
+        expect(result.errorCode).to.equal(0);
+    });
+
+    it('didManagerRemoveKey it', async () => {
+        const result = await mainApp.executeAllPluginsMethods(dm, 'DidManagerRemoveKey',jsonData.did,jsonData.extension[0]);
+        // 断言
+        expect(result.errorCode).to.equal(0);
+    });
+
+    it('didManagerAddService it', async () => {
+        const serviceData = {
+            "id": "msg4asdsadsdsa",
+            "type": "DIDCommMessaging",
+            "serviceEndpoint": [
+                {
+                    "uri": "http://localhost:3002/messaging",
+                    "routingKeys": [
+                        "${mediator2.did}#${mediator2.keys[0].kid}",
+                        "${mediator.did}#${mediator.keys[0].kid}"
+                    ]
+                }
+            ]
+        }
+        const result = await mainApp.executeAllPluginsMethods(dm, 'DidManagerAddService',jsonData.did,serviceData);
+        // 断言
+        expect(result.errorCode).to.equal(0);
+    });
+
+    it('didManagerRemoveService it', async () => {
+        const result = await mainApp.executeAllPluginsMethods(dm, 'DidManagerRemoveService',jsonData.did,"did:fake:receiverWithMediation2");
+        // 断言
+        expect(result.errorCode).to.equal(0);
+    });
+
+    it('didManagerFind it', async () => {
+        const result = await mainApp.executeAllPluginsMethods(dm, 'DidManagerFind',jsonData.did);
+        // 断言
+        expect(result.errorCode).to.equal(0);
+    });
+
+    it('delete didManagerDelete', async () => {
+        // 从数据库中检索保存的 DidDocument
+        const result = await mainApp.executeAllPluginsMethods(dm, 'DidManagerDelete',jsonData.did);
+        // 断言
+        expect(result.errorCode).to.equal(0);
+    });
+
+});
+
+
 
 
 
